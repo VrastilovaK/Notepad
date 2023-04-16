5efe4cfd78b391365937e18bd265a9bc4ff878ca
VrastilovaK <vrastilova.kristyna@gmail.com>
2023-04-16 21:24:55 +0200

    main window + icon

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # coding = UTF8
+import customtkinter
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Notepad")
+        self.iconbitmap("Notepad.ico")
+        self.geometry("800x600")
+
+
+# mainloop
+app = App()
+app.mainloop()
