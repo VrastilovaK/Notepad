3aef4c0db09447a5b0647450d9bd77623c48057a
VrastilovaK <vrastilova.kristyna@gmail.com>
2023-04-21 13:29:45 +0200

    edit and delete note method created

--- main.py
+++ main.py
@@ -4,43 +4,6 @@ import customtkinter
 import json
 
 
-# new note
-class NewNote(customtkinter.CTkToplevel):
-    def __init__(self, master):
-        super().__init__(master)
-        self.geometry("400x340+250+250")
-        self.title("New note")
-        self.resizable(False, False)
-
-        self.title_label = customtkinter.CTkLabel(self, text="Title: ")
-        self.title_label.grid(row=0, column=0, pady=10)
-
-        self.title = customtkinter.CTkEntry(self)
-        self.title.grid(row=0, column=1, pady=10)
-
-        self.content_label = customtkinter.CTkLabel(self, text="Content: ")
-        self.content_label.grid(row=1, column=0, columnspan=2, pady=(0, 10))
-
-        self.textbox = customtkinter.CTkTextbox(self)
-        self.textbox.configure(width=380)
-        self.textbox.grid(row=2, column=0, columnspan=2, pady=(0, 10), padx=10)
-
-        self.save_button = customtkinter.CTkButton(self, text="Save", command=self.save_note)
-        self.save_button.grid(row=3, column=0, columnspan=2, pady=(0, 10))
-
-    def save_note(self):
-        title_save = self.title.get()
-        content = self.textbox.get("0.0", tkinter.END)
-        notes[title_save] = content.strip()
-
-        with open("notes.json", "w") as f:
-            json.dump(notes, f)
-
-        app.load_notes()
-        self.destroy()
-
-
-# menu with buttons
 class Menu(customtkinter.CTkFrame):
     def __init__(self, master, width):
         super().__init__(master, width)
@@ -71,10 +34,19 @@ class Menu(customtkinter.CTkFrame):
             self.new_note_window.focus()
 
     def edit_note(self):
-        pass
+        if self.edit_note_window is None or not self.edit_note_window.winfo_exists():
+            self.edit_note_window = EditNote(self)
+            self.edit_note_window.grab_set()
+        else:
+            self.edit_note_window.focus()
 
     def delete_note(self):
-        pass
+        note = app.note_view.get()
+        app.note_view.delete(note)
+        notes.pop(note)
+        with open("notes.json", "w") as f:
+            json.dump(notes, f)
+        app.load_notes()
 
     def delete_all(self):
         notes.clear()
@@ -83,7 +55,6 @@ class Menu(customtkinter.CTkFrame):
         app.load_notes()
 
 
-# frame for saved notes
 class NoteView(customtkinter.CTkTabview):
     def __init__(self, master, width):
         super().__init__(master, width)
@@ -91,7 +62,84 @@ class NoteView(customtkinter.CTkTabview):
         self.configure(width=self.master_width - 10, height=400)
 
 
-# footer with date and time
+class NewNote(customtkinter.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.geometry("400x340+250+250")
+        self.title("New note")
+        self.resizable(False, False)
+
+        self.title_label = customtkinter.CTkLabel(self, text="Title: ")
+        self.title_label.grid(row=0, column=0, pady=10)
+
+        self.title = customtkinter.CTkEntry(self)
+        self.title.grid(row=0, column=1, pady=10)
+
+        self.content_label = customtkinter.CTkLabel(self, text="Content: ")
+        self.content_label.grid(row=1, column=0, columnspan=2, pady=(0, 10))
+
+        self.textbox = customtkinter.CTkTextbox(self)
+        self.textbox.configure(width=380)
+        self.textbox.grid(row=2, column=0, columnspan=2, pady=(0, 10), padx=10)
+
+        self.save_button = customtkinter.CTkButton(self, text="Save", command=self.save_note)
+        self.save_button.grid(row=3, column=0, columnspan=2, pady=(0, 10))
+
+    def save_note(self):
+        title_save = self.title.get()
+        content = self.textbox.get(0.0, tkinter.END)
+        notes[title_save] = content.strip()
+
+        with open("notes.json", "w") as f:
+            json.dump(notes, f)
+
+        app.load_notes()
+        self.destroy()
+
+
+class EditNote(customtkinter.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.geometry("400x340+250+250")
+        self.title("Edit note")
+        self.resizable(False, False)
+        self.curent_note = app.note_view.get()
+        self.curent_text = self.load_note()
+
+        self.title_label = customtkinter.CTkLabel(self, text="Title:")
+        self.title_label.grid(row=0, column=0, pady=10)
+
+        self.title = customtkinter.CTkLabel(self, text=self.curent_note)
+        self.title.grid(row=0, column=1, pady=10)
+
+        self.content_label = customtkinter.CTkLabel(self, text="Content: ")
+        self.content_label.grid(row=1, column=0, columnspan=2, pady=(0, 10))
+
+        self.textbox = customtkinter.CTkTextbox(self)
+        self.textbox.configure(width=380)
+        self.textbox.grid(row=2, column=0, columnspan=2, pady=(0, 10), padx=10)
+        self.textbox.insert("0.0", f"{self.curent_text}")
+
+        self.save_button = customtkinter.CTkButton(self, text="Save", command=self.save_note)
+        self.save_button.grid(row=3, column=0, columnspan=2, pady=(0, 10))
+
+    def load_note(self):
+        with open("notes.json", "w") as f:
+            json.dump(notes, f)
+        note_text = notes.get(self.curent_note)
+        return note_text
+
+    def save_note(self):
+        content = self.textbox.get(0.0, tkinter.END)
+        notes[self.curent_note] = content.strip()
+
+        with open("notes.json", "w") as f:
+            json.dump(notes, f)
+
+        app.load_notes()
+        self.destroy()
+
+
 class Footer(customtkinter.CTkFrame):
     def __init__(self, master, width):
         super().__init__(master, width)
@@ -99,10 +147,9 @@ class Footer(customtkinter.CTkFrame):
         self.configure(width=self.master_width, height=30)
 
         self.label = customtkinter.CTkLabel(self, text="Notepad by Tewe")
-        self.label.grid(row=0, column=0, padx=(30, 100))
+        self.label.grid(row=0, column=0, padx=15)
 
 
-# app
 class App(customtkinter.CTk):
     def __init__(self, ):
         super().__init__()
@@ -119,7 +166,7 @@ class App(customtkinter.CTk):
         self.note_view.grid(row=1, column=0, columnspan=15, padx=5)
 
         self.footer = Footer(master=self, width=self.width)
-        self.footer.place(y=self.height - 40, x=200)
+        self.footer.grid(row=2, column=0, columnspan=15, padx=5, pady=15)
 
     def load_notes(self):
         try:
@@ -132,12 +179,12 @@ class App(customtkinter.CTk):
 
             for title, content in load_notes.items():
                 try:
-                    self.note_view.add(f"{title}")
-                    self.note_view.label = customtkinter.CTkLabel(master=self.note_view.tab(f"{title}"), text=content)
+                    self.note_view.add(title)
+                    self.note_view.label = customtkinter.CTkLabel(master=self.note_view.tab(title), text=content, justify="left")
                     self.note_view.label.grid(row=0, column=0, padx=20, pady=10)
 
                 except ValueError:
-                    self.note_view.label = customtkinter.CTkLabel(master=self.note_view.tab(f"{title}"), text=content)
+                    self.note_view.label = customtkinter.CTkLabel(master=self.note_view.tab(title), text=content)
                     self.note_view.label.grid(row=0, column=0, padx=20, pady=10)
 
         except FileNotFoundError:
@@ -146,7 +193,6 @@ class App(customtkinter.CTk):
 
 # mainloop
 notes = {}
-
 try:
     with open("notes.json", "r") as file:
         notes = json.load(file)
